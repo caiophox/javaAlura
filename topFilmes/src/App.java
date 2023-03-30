3f97418c32ab2fe5628daa1a9a37b8d728a0d2a7
Caio Phox <92237575+caiophox@users.noreply.github.com>
2023-03-30 20:41:20 -0300

    Versão final da aula 1

--- topFilmes/src/App.java
+++ topFilmes/src/App.java
@@ -3,15 +3,29 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        //fazer conexão HTTP e buscar 250 filmes no topo
         String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
         URI adressURI = URI.create(url);
         HttpClient clientProtocol = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder(adressURI).GET().build();
         HttpResponse<String> response = clientProtocol.send(request, BodyHandlers.ofString());
         String body = response.body();
-        System.out.println(body);
+
+        // Extração de dados relevantes
+        var parser = new JsonParser();
+        List<Map<String, String>> movieList = parser.parse(body) ;
+
+        //Exibir os itens
+        for (Map<String,String> filme : movieList) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
     }
 }
