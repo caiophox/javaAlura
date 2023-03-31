f15ec6556ee089e36a0e132cba79c6bdfc2ca1a5
Caio Phox <92237575+caiophox@users.noreply.github.com>
2023-03-31 19:02:24 -0300

    Aula 2 completa

--- topFilmes/src/App.java
+++ topFilmes/src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -7,24 +9,35 @@ import java.util.List;
 import java.util.Map;
 
 public class App {
+
     public static void main(String[] args) throws Exception {
-        //fazer conexão HTTP e buscar 250 filmes no topo
+
+        // fazer uma conexão HTTP e buscar os top 250 filmes
+        //String url = "https://imdb-api.com/en/API/Top250Movies/k_0ojt0yvm";
         String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
-        URI adressURI = URI.create(url);
-        HttpClient clientProtocol = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder(adressURI).GET().build();
-        HttpResponse<String> response = clientProtocol.send(request, BodyHandlers.ofString());
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
 
-        // Extração de dados relevantes
+        // extrair só os dados que interessam (titulo, poster, classificação)
         var parser = new JsonParser();
-        List<Map<String, String>> movieList = parser.parse(body) ;
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
-        //Exibir os itens
-        for (Map<String,String> filme : movieList) {
-            System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
-            System.out.println(filme.get("imDbRating"));
+        // exibir e manipular os dados 
+        var geradora = new GeradoraDeFigurinhas();
+        for (Map<String,String> filme : listaDeFilmes) {
+
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nomeArquivo = titulo + ".png";
+
+            geradora.cria(inputStream, nomeArquivo);
+
+            System.out.println(titulo);
             System.out.println();
         }
     }
--- topFilmes/src/JsonParser.java
+++ topFilmes/src/JsonParser.java
@@ -6,34 +6,36 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class JsonParser {
-  private static final Pattern REGEX_ITEMS = Pattern.compile(".*\\[(.+)\\].*");
-  private static final Pattern REGEX_ATRIBUTOS_JSON = Pattern.compile("\"(.+?)\":\"(.*?)\"");
+    
+    private static final Pattern REGEX_ITEMS = Pattern.compile(".*\\[(.+)\\].*");
+    private static final Pattern REGEX_ATRIBUTOS_JSON = Pattern.compile("\"(.+?)\":\"(.*?)\"");
 
-  public List<Map<String, String>> parse(String json) {
-    Matcher matcher = REGEX_ITEMS.matcher(json);
-          if (!matcher.find()) {
+    public List<Map<String, String>> parse(String json) {
+        Matcher matcher = REGEX_ITEMS.matcher(json);
+        if (!matcher.find()) {
 
-              throw new IllegalArgumentException("Não encontrou items.");
-          }
+            throw new IllegalArgumentException("Não encontrou items.");
+        }
 
-          String[] items = matcher.group(1).split("\\},\\{");
+        String[] items = matcher.group(1).split("\\},\\{");
 
-          List<Map<String, String>> dados = new ArrayList<>();
+        List<Map<String, String>> dados = new ArrayList<>();
 
-          for (String item : items) {
+        for (String item : items) {
 
-              Map<String, String> atributosItem = new HashMap<>();
+            Map<String, String> atributosItem = new HashMap<>();
 
-              Matcher matcherAtributosJson = REGEX_ATRIBUTOS_JSON.matcher(item);
-              while (matcherAtributosJson.find()) {
-                  String atributo = matcherAtributosJson.group(1);
-                  String valor = matcherAtributosJson.group(2);
-                  atributosItem.put(atributo, valor);
-              }
+            Matcher matcherAtributosJson = REGEX_ATRIBUTOS_JSON.matcher(item);
+            while (matcherAtributosJson.find()) {
+                String atributo = matcherAtributosJson.group(1);
+                String valor = matcherAtributosJson.group(2);
+                atributosItem.put(atributo, valor);
+            }
 
-              dados.add(atributosItem);
-          }
+            dados.add(atributosItem);
+        }
 
-          return dados;
-  }
+        return dados;
+    } 
+
 }
